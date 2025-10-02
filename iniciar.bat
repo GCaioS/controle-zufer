53af96b8ef25b7f2e4bcd1d45d1aeef83d67045c
Caio <109152281+GCaioS@users.noreply.github.com>
2025-10-02 15:14:13 +0000

    teste

--- iniciar.bat
+++ iniciar.bat
@@ -1,6 +1,6 @@
 @echo off
-REM Inicia o servidor Node.js e abre o navegador automaticamente
-start "Servidor" cmd /c "node server.js"
+REM Inicia o servidor Node.js em segundo plano sem mostrar terminal
+start /min /b node server.js
 REM Aguarda alguns segundos para o servidor iniciar
 ping 127.0.0.1 -n 3 > nul
 start http://localhost:3000
