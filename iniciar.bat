68b89904a5d2c9c2492bc9d72ba08cdca93b1bf8
Caio <109152281+GCaioS@users.noreply.github.com>
2025-10-02 15:45:03 +0000

    Envio

--- iniciar.bat
+++ iniciar.bat
@@ -3,4 +3,3 @@ REM Inicia o servidor Node.js em segundo plano sem mostrar terminal
 start /min /b node server.js
 REM Aguarda alguns segundos para o servidor iniciar
 ping 127.0.0.1 -n 3 > nul
-start http://localhost:3000
